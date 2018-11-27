f2272c2c1c0e3058ea73761ef69e52550f979fab
Roliga <roliga.here@gamil.com>
2018-11-27 01:48:15 +0100

    [BandcampBridge] Add artist feed (#581)
    Initial messy implementation

--- bridges/BandcampBridge.php
+++ bridges/BandcampBridge.php
@@ -6,19 +6,110 @@ class BandcampBridge extends BridgeAbstract {
 	const URI = 'https://bandcamp.com/';
 	const CACHE_TIMEOUT = 600; // 10min
 	const DESCRIPTION = 'New bandcamp release by tag';
-	const PARAMETERS = array( array(
-		'tag' => array(
-			'name' => 'tag',
-			'type' => 'text',
-			'required' => true
+	const PARAMETERS = array(
+		'By tag' => array(
+			'tag' => array(
+				'name' => 'tag',
+				'type' => 'text',
+				'required' => true
+			)
+		),
+		'Band releases' => array(
+			'band' => array(
+				'name' => 'band',
+				'type' => 'text',
+				'required' => true
+			)
 		)
-	));
+	);
 
 	public function getIcon() {
 		return 'https://s4.bcbits.com/img/bc_favicon.ico';
 	}
 
+	private function parseAlbumPage($html){
+		foreach($html->find('script[type="text/javascript"]') as $script) {
+			$regex = '/var TralbumData = {(.+?)};/s';
+			if(preg_match($regex, $script->innertext, $matches) > 0) {
+				$albumDataJs = $matches[1];
+				break;
+			}
+		}
+
+		$albumData = array();
+		$regex = '/^ *([a-zA-Z_]+?):(.+?),?$/m';
+		preg_match_all($regex, $albumDataJs, $jsObjects, PREG_SET_ORDER);
+		foreach($jsObjects as $jsObject) {
+			$albumData[$jsObject[1]] = json_decode($jsObject[2]);
+		}
+
+		return (object)$albumData;
+	}
+
+	private function collectArtistData(){
+		$html = getSimpleHTMLDOM($this->getURI() . '/music',
+			array(),
+			array(),
+			true,
+			true,
+			DEFAULT_TARGET_CHARSET,
+			false); // Don't strip newlines, they're used when parsing album pages
+
+		// Album list page, eg. https://tlrvt.bandcamp.com/music
+		$albumData = $html->find('ol.music-grid', 0);
+		if(isset($albumData)) {
+			$albumListJSON = json_decode(htmlspecialchars_decode($albumData->getAttribute('data-initial-values')));
+			foreach($albumListJSON as $album) {
+				if(isset($album->artist)) {
+					$albumArtist = $album->artist;
+				} else {
+					$albumArtist = $album->band_name;
+				}
+				$albumURI = $this->getURI() . $album->page_url;
+
+				$item = array();
+				$item['uri'] = $albumURI;
+
+				$item['author'] = $albumArtist;
+				$item['title'] = $albumArtist . ' - ' . $album->title;
+				$item['timestamp'] = strtotime($album->publish_date);
+				$item['content'] = '<img src="https://f4.bcbits.com/img/a'
+				. $album->art_id
+				. '_2.jpg"/><br/>'
+				. $albumArtist
+				. ' - '
+				. $album->title;
+				$this->items[] = $item;
+			}
+		}
+
+		// Releases page, eg. https://parallelspec.bandcamp.com/releases
+		$albumData = $this->parseAlbumPage($html);
+		if(empty($albumData)) {
+			returnServerError('No albums found for this artist');
+		} else {
+			$item = array();
+			$item['uri'] = $albumData->url;
+
+			$item['author'] = $albumData->artist;
+			$item['title'] = $albumData->artist . ' - ' . $albumData->current->title;
+			$item['timestamp'] = strtotime($albumData->current->publish_date);
+			$item['content'] = '<img src="https://f4.bcbits.com/img/a'
+			. $albumData->art_id
+			. '_2.jpg"/><br/>'
+			. $albumData->artist
+			. ' - '
+			. $albumData->current->title;
+			$this->items[] = $item;
+		}
+	}
+
 	public function collectData(){
+		if($this->queriedContext === 'Band releases') {
+			$this->collectArtistData();
+			return;
+		}
+
 		$html = getSimpleHTMLDOM($this->getURI())
 			or returnServerError('No results for this query.');
 
@@ -50,11 +141,17 @@ class BandcampBridge extends BridgeAbstract {
 	}
 
 	public function getURI(){
-		if(!is_null($this->getInput('tag'))) {
-			return self::URI . 'tag/' . urlencode($this->getInput('tag')) . '?sort_field=date';
+		switch($this->queriedContext) {
+		case 'Band releases':
+			if(!is_null($this->getInput('band'))) {
+				return 'https://' . $this->getInput('band') . '.bandcamp.com';
+			}
+		case 'By tag':
+			if(!is_null($this->getInput('tag'))) {
+				return self::URI . 'tag/' . urlencode($this->getInput('tag')) . '?sort_field=date';
+			}
+		default: return parent::getURI();
 		}
-
-		return parent::getURI();
 	}
 
 	public function getName(){
